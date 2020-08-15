98bac1bcefeb8df393479b11a7987180155feadf
Watanaporn ruamsuk <12469097+wathanaporn@users.noreply.github.com>
2020-08-15 14:53:33 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: 'AIzaSyAzeZKIdmQS1kbM9ANcgfTVVRdd5Sow3fg',
-    authDomain: 'new-accounts-93b33.firebaseapp.com',
-    databaseURL: 'https://new-accounts-93b33.firebaseio.com',
-    projectId: 'new-accounts-93b33',
-    storageBucket: 'new-accounts-93b33.appspot.com',
-    messagingSenderId: '103514079123',
-    appId: '1:103514079123:web:03140b8cb312fae206b266',
-    measurementId: 'G-QXRG4M2BRL'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
 };
 
